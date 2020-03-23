5dc109f2f0d1a3b623ab9a96686082e04fc09921
Michael Chan <michael.chan@mercari.com>
2020-03-23 09:43:25 +0900

    Initial Commit

---
+++ COVID-19 WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  COVID-19 WatchKit Extension
+//
+//  Created by michael.chan on 23/03/2020.
+//  Copyright © 2020 michael.chan. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
---
+++ COVID-19 WatchKit Extension/NotificationView.swift
@@ -0,0 +1,21 @@
+//
+//  NotificationView.swift
+//  COVID-19 WatchKit Extension
+//
+//  Created by michael.chan on 23/03/2020.
+//  Copyright © 2020 michael.chan. All rights reserved.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
